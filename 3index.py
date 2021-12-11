48ae969caf50553869f38aa0c58ca9f7d67ed166
Zak1r <94119532+Zak1r@users.noreply.github.com>
2021-12-11 13:34:20 +0600

    4th hw done

--- 3index.py
+++ 3index.py
@@ -1,1 +1,7 @@
 #task 4
+def convertTemp(degree, value1, value2):
+    if value1 == 'C':   return (degree * (9 / 5) + 32)
+    else:   return(degree - 32) * (5 / 9)
+
+print(convertTemp(86, 'F', 'C'))
+print(convertTemp(30, 'C', 'F'))
