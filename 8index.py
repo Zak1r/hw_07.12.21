b9604962bc85877f295d8e7d0987c133edbcbc1c
Zak1r <94119532+Zak1r@users.noreply.github.com>
2021-12-11 13:46:56 +0600

    9th task done

--- 8index.py
+++ 8index.py
@@ -1,1 +1,16 @@
 #task 9
+
+def endZeros(num: int):
+    zeros = 0
+    num = str(num)
+    if num [-1] != '0': return 0
+    
+    for i in range(len(num) -1, -1, -1):
+        if num[i] == 0: zeros += 1
+        else:   return zeros
+    return zeros
+
+print(endZeros(0))
+print(endZeros(1))
+print(endZeros(10))
+print(endZeros(101))
